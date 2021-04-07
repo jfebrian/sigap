dfe4cf5c9d61a1d954bb595321396fbfe4c62afb
Joanda Febrian <febrianjoanda@gmail.com>
2021-04-07 21:40:46 +0700

    feat: Add CloudKit integration to Area Code Authentication

--- sigap/Authentication/AuthViewController.swift
+++ sigap/Authentication/AuthViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import CloudKit
 
 class AuthViewController: UIViewController, UITextFieldDelegate  {
 
     
     @IBOutlet weak var areaCodeTextField: UITextField!
     @IBOutlet weak var areaImage: UIImageView!
+    @IBOutlet weak var areaNameLabel: UILabel!
+    
+    let publicDatabase = CKContainer.default().publicCloudDatabase
+    let areaRecordType = "Areas"
+    
+    var area: CKRecord?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,26 +27,51 @@ class AuthViewController: UIViewController, UITextFieldDelegate  {
         
         switch restorationIdentifier {
         case "areaCodeSb":
+            self.areaCodeTextField.returnKeyType = .go
             self.areaCodeTextField.delegate = self
         case "welcomeSb":
+            self.areaNameLabel.text = area?.value(forKey: "name") as? String
             self.areaImage.layer.cornerRadius = 15
+            if let asset = area?.value(forKey: "image") as? CKAsset,
+               let data = try? Data(contentsOf: (asset.fileURL!)),
+               let image = UIImage(data: data)
+            {
+                self.areaImage.image = image
+            }
+            
         default:
             break
         }
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == areaCodeTextField {
-            textField.resignFirstResponder()
-            areaCodeReturned()
+        if textField == areaCodeTextField, textField.text != "" {
+            let predicate = NSPredicate(format: "code = %@", textField.text!)
+            let query = CKQuery(recordType: areaRecordType, predicate: predicate)
+            publicDatabase.perform(query, inZoneWith: nil) { (results, error) in
+                if error != nil {
+                    print("error: \(error!.localizedDescription)")
+                } else {
+                    if results!.count == 0 {
+                        print("code not found")
+                    } else {
+                        DispatchQueue.main.async {
+                            textField.resignFirstResponder()
+                            self.performSegue(withIdentifier: "validAreaCode", sender: results)
+                        }
+                    }
+                }
+            }
             return false
         }
         return true
     }
-
-    func areaCodeReturned() {
-        performSegue(withIdentifier: "validAreaCode", sender: self)
-    }
     
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "validAreaCode" {
+            let destination = segue.destination as! AuthViewController
+            let sentArea = sender as! [CKRecord]
+            destination.area = sentArea[0]
+        }
+    }
 }
