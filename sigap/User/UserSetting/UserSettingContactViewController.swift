8e7c23fb235dbf3b27d298073fb96e9d4d55d8bc
muhammad rifki widadi <rifki.widadi29@gmail.com>
2021-04-12 23:06:59 +0700

    feat: add user setting language & logout

---
+++ sigap/User/UserSetting/UserSettingContactViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UserSettingContactViewController.swift
+//  sigap
+//
+//  Created by Muhammad Rifki Widadi on 12/04/21.
+//
+
+import UIKit
+
+class UserSettingContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Contact"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
