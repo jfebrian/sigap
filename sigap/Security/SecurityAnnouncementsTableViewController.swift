fa2039935fac8e1d2b51ccdd03066e3db7a21b48
Joanda Febrian <febrianjoanda@gmail.com>
2021-04-12 15:23:44 +0700

    fix merge

--- sigap/Security/SecurityAnnouncementsTableViewController.swift
+++ sigap/Security/SecurityAnnouncementsTableViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class SecurityAnnouncementsTableViewController: UITableViewController {
+    
+    //Structure data to send
+    struct dataToSend {
+        let title: String
+        let date: String
+        let content: String
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +28,34 @@ class SecurityAnnouncementsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "announcementId") as! SecurityAnnouncementsTableViewCell
         
-        cell.titleLabel.text = "Penutupan Gerbang Utara Pasca Lockdown Pandemi"
+        cell.titleLabel.text = "Judul Baru"
         cell.contentLabel.text = "Kami pihak security dari perumahan Taman Setia Budi Indah sepakat untuk menutup gerbang utara untuk meningkatkan prosedur keamanan dari komplek ini. Dimohon untuk seluruh warga agar dapat menggunakan pintu gerbang selatan selama lockdown pandemi ini."
         cell.dateLabel.text = generateDate(daysBack: indexPath.row)
 
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.cellForRow(at: indexPath) as! SecurityAnnouncementsTableViewCell
+        let data = dataToSend(title: cell.titleLabel.text!, date: cell.dateLabel.text!, content: cell.contentLabel.text!)
+        performSegue(withIdentifier: "detailSegue", sender: data)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailSegue"{
+            let destination = segue.destination as! SecurityAnnouncementDetailsViewController
+            let data = sender as! dataToSend
+            
+            destination.titleString = data.title
+            destination.dateString = data.date
+            destination.contentString = data.content
+        }
+    }
+    
+    
+    
     func generateDate(daysBack: Int)-> String{
         let day = daysBack
         let today = Date(timeIntervalSinceNow: 0)
